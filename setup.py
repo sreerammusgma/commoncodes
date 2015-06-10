56e395e4bf753cbfb63b14775ed13a57add5517d
sreeram.menon <sreeram.menon@mu-sigma.com>
2015-06-10 18:42:59 +0530

    updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='commoncodes',
+      version='1.0',
+      description='The venus commoncodes',
+      url='https://github.com/sreerammusgma/commoncodes',
+      author='Flying Circus',
+      author_email='sreeram.menon@example.com',
+      license='Mu-Sigma',
+      packages=['commoncodes'],
+      zip_safe=False)
